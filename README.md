07c4ae00da3e773d39825f1b73bb152149b6b6b6
Inga Feick <inga.feick@trivago.com>
2022-09-06 17:40:18 +0200

    Convert inches

--- README.md
+++ README.md
@@ -8,20 +8,23 @@ To check if the installation works in your browser, navigate to the [test page](
 
 ## TODOs
 
-* convert mph and miles
+* convert mph
+* convert acres
 * convert inch, foot, yard, mile
-* barrels
+* convert barrels
 * units for cooking
 * remove console output
-* support for floats
 * maybe replace jquery with pure js
 * publish
 * test if this works on pages where content is added after initial dom is created
-* more units up for convertion at
-** [wiki/United_States_customary_units](https://simple.wikipedia.org/wiki/United_States_customary_units)
-** [wiki/Category:Imperial_units](https://en.wikipedia.org/wiki/Category:Imperial_units)
 
 ## Notes for development
 
-* Regex for the temperature in [ecma](https://regex101.com/r/Wrpp4x/1) and the original in [pcre](https://regex101.com/r/Ak5Joj/1)
+* Regexes:
+** temperature in [ecma](https://regex101.com/r/Wrpp4x/1) and the original in [pcre](https://regex101.com/r/Ak5Joj/1)
+** inches https://regex101.com/r/Cjhgl4/1
+** feet
 * `npm install regex-translator; npx regex-translator -o -T ecma -F pcre -R "(° ?)?\d+(\.\d+)?+°?+ ?[fF]"` for converting regexes from pcre 🧠 => ecma 💪
+* more units up for convertion at
+** [wiki/United_States_customary_units](https://simple.wikipedia.org/wiki/United_States_customary_units)
+** [wiki/Category:Imperial_units](https://en.wikipedia.org/wiki/Category:Imperial_units)
--- eurofox.js
+++ eurofox.js
@@ -1,35 +1,46 @@
-/* Original code lives at https://github.com/spb/tc */
-
-// TODO replace size units as well
-
 var regex_temperature = /(° ?)?[0-9]+(\.[0-9]+)? ?°? ?[fF]/g
+var regex_inch = /(° ?)?[0-9]+(\.[0-9]+)? ?°? ?in(ch)?/g
 
-function clean_temperature(input){
+function cleanTemperature(input){
   return input.replace(/[Ff]$/, '').replace('°','').replace(' ','')
 }
 
-function to_celsius(input) {
-    var f = parseInt(clean_temperature(input), 10); // TODO convert to float
+function cleanInch(input){
+  return input.replace('inch', '').replace('in','').replace(' ','')
+}
+
+function toCelsius(input) {
+    var f = parseFloat(cleanTemperature(input), 10);
     c = Math.floor((f - 32) / 1.8);
     return c + "° C"
+}
+
+function toCentimeters(input) {
+    var f = parseFloat(cleanInch(input), 10);
+    return Math.floor(f * 2.54) + "cm"
 }
 
 function textNodeFilter() {
     return this.nodeType == 3 && this.nodeName.toLowerCase() != 'script' && this.nodeName.toLowerCase() != 'style'
 }
 
+function makeNewText(original, replacement){
+    return '<span title="' + original + '">' + replacement + '</span>'
+}
 
-console.log("Begin substitutions");
 $("body").find("*").contents().filter(textNodeFilter).each(function(index) {
     var textNode = $(this);
     var nodeParent = textNode.parent()[0].localName
     var text = textNode.text();
+    console.log("Node " + nodeParent + " with text '" + text);
     matches = text.match(regex_temperature);
-    console.log("Node " + nodeParent + " with text '" + text + " => matches: " + matches);
+    // console.log(" => temperature matches: " + matches);
     if (matches) {
-        var original = matches[0];
-        var celsius = to_celsius(matches[0]);
-        textNode.replaceWith(text.replace(regex_temperature, '<span title="' + original + '">' + celsius + '</span>'))
+        textNode.replaceWith(text.replace(regex_temperature, makeNewText(matches[0], toCelsius(matches[0]))))
+    }
+    matches = text.match(regex_inch);
+    console.log(" => inch matches: " + matches);
+    if (matches) {
+        textNode.replaceWith(text.replace(regex_inch, makeNewText(matches[0], toCentimeters(matches[0]))))
     }
 });
-console.log("End substitutions");
