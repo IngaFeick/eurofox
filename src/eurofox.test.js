279b19c2729ab92fe31bc0301241d011df261b3e
Inga Feick <inga.feick@trivago.com>
2022-09-07 17:12:17 +0200

    Babys first test case uwu

--- src/eurofox.test.js
+++ src/eurofox.test.js
@@ -3,3 +3,9 @@ const {hello, translate2european} = require('./eurofox');
 test("Hello", () => {
     expect(hello()).toBe("hello");
 });
+
+
+
+test("translate2european", () => {
+    expect(translate2european("9 inch nails")).toBe('<span title="9 inch">23 cm</span> nails');
+});
